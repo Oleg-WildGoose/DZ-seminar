f2bd4d283c7e42eeb6eb489b8c4115ef2579b4b6
oleg <ojiegoff@gmail.com>
2022-09-10 23:02:27 +0500

    ДЗ4

---
+++ DZ4/Program.cs
@@ -0,0 +1,5 @@
+﻿int N, i;                             
+    Console.WriteLine("введите число: ");
+     N = Convert.ToInt32(Console.ReadLine());
+for (i = 0; i<= N; i+=2)
+    Console.Write(i + "  ");

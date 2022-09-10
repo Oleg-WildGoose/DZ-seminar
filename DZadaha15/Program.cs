750fccda6d4fe6eae2b35265df9e6e13ed43a9bc
oleg <ojiegoff@gmail.com>
2022-09-10 23:41:40 +0500

    Задача 15

---
+++ DZadaha15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int NumberDay = Convert.ToInt32(Console.ReadLine());
+
+{
+  if (NumberDay == 6 || NumberDay == 7) 
+  {
+  Console.WriteLine("выходной");
+  }
+  else if (NumberDay < 1 || NumberDay > 7) 
+  {
+    Console.WriteLine("не день недели");
+  }
+  else Console.WriteLine("не выходной");
+}

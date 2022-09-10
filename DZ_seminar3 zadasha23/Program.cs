f66b2fdfc414b8702d68567498b5bcf1547f7143
oleg <ojiegoff@gmail.com>
2022-09-11 01:11:01 +0500

    Задача 23

---
+++ DZ_seminar3 zadasha23/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+void Cube(int[] a){
+  int b = 0;
+  int length = a.Length;
+  while (b <  length){
+    a[b] = Convert.ToInt32(Math.Pow(b, 3));
+    b++;
+  }
+}
+void PrintArry(int[] coll){
+  int b = coll.Length;
+  int i = 0;
+  while(i < b){
+    Console.Write(coll[i]+ " ");
+    i++;
+  }
+} 
+
+int[] arry = new int[a+1];
+Cube(arry);
+PrintArry(arry);

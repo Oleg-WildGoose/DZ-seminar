00a751ea140308b70d5af05a7b7bd540af369f9a
oleg <ojiegoff@gmail.com>
2022-09-10 22:06:21 +0500

    ДЗ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введи 2 числа и там посмотрим");
+            Console.Write("Введи   число 1 - ");
+            int a = Convert.ToInt32(Console.Read());
+            Console.Write("Введи   число 2 - ");
+            int b = Convert.ToInt32(Console.Read());
+            if ( a > b)
+            {
+                Console.WriteLine($"Число {a} больше чем {b}");
+            }
+            else if (a < b )
+            {
+                Console.WriteLine($"Число {b} больше чем {a}");
+            }
+           

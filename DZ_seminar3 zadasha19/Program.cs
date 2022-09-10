b27a676dca589ab529415e841a02e39eb761eafe
oleg <ojiegoff@gmail.com>
2022-09-11 00:46:25 +0500

    Задача 19

---
+++ DZ_seminar3 zadasha19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+string? num = Console.ReadLine();
+
+void Number(string num)
+{
+  if (num[0]==num[4] || num[1]==num[3])
+  {
+    Console.WriteLine("Ваше число палиндром.");
+  }
+  else Console.WriteLine("Ваше число не палиндром.");
+}
+
+if (num!.Length == 5)
+{
+  Number(num);
+}
+else Console.WriteLine("Введи другое число");

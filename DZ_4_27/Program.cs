5b4b6fa6765bf140cc92e9ffed6911b8a1fe58a2
oleg <ojiegoff@gmail.com>
2022-09-25 13:01:08 +0500

    Задача 27/1

---
+++ DZ_4_27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число - ");
+int N = Convert.ToInt32(Console.ReadLine());
+    int SumNumber(int N)
+    {
+    int cn = Convert.ToString(N).Length;
+    int prog = 0;
+    int result = 0;
+        for (int i = 0; i < cn; i++)
+        {
+        prog = N - N % 10;
+        result = result + (N - prog);
+        N = N / 10;
+        }
+    return result;
+    }
+int sumN = SumNumber(N);
+Console.WriteLine("Сумма цифр в числе - " + sumN);

85a2abfee5fb66acaf7ab6d1a42bd81fdf7c46bb
oleg <ojiegoff@gmail.com>
2022-09-25 12:48:22 +0500

    Задача 25/1

---
+++ DZ_4_25/Program.cs
@@ -0,0 +1,16 @@
+﻿int Exponentiation(int A, int B)
+    {
+    int result = 1;
+    for(int i=1; i <= B; i++)
+    {
+    result = result * A;
+    }
+    return result;
+    }
+  Console.Write("Введите число A - ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B - ");
+  int B = Convert.ToInt32(Console.ReadLine());
+
+  int expo = Exponentiation(A, B);
+  Console.WriteLine("Ответ - " + expo);

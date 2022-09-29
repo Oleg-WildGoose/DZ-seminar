844af9ad67c0d33d011e79aa687b7bb54e891d54
oleg <ojiegoff@gmail.com>
2022-09-30 03:44:03 +0500

    Задача 38

---
+++ DZ_5_38/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] num = new double[size];
+FillArrayRandomNumbers(num);
+Console.WriteLine("Массив: ");
+PrintArray(num);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int a = 0; a < num.Length; a++)
+{
+    if (num[a] > max)
+        {
+        max = num[a];
+        }
+    if (num[a] < min)
+        {
+        min = num[a];
+        }
+}
+
+Console.WriteLine($"итого {num.Length}. Макс. значение - {max}, мин. значение - {min}");
+Console.WriteLine($"Разница между макс. и мин. значениями - {max - min}");
+
+void FillArrayRandomNumbers(double[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+        {
+        num[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+        {
+        Console.Write(num[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}

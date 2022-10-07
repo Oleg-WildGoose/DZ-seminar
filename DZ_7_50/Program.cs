148a82a6ce2813d2057f680d835cd30bbedb7f96
oleg <ojiegoff@gmail.com>
2022-10-07 18:37:19 +0500

    Задача 7/50

---
+++ DZ_7_50/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Для вывода данных необходимо указать параметры запроса от 1 до 10");
+Console.WriteLine("введите номер строки - ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца - ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет, интервал составляет от 1 до 10");
+}
+else
+{
+    Console.WriteLine("Собственно сам массив из потока бреда");
+    PrintArray(numbers);
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}

dbb6d671c10ca54fdbf2d5af64f7b3c998b20ed3
oleg <ojiegoff@gmail.com>
2022-10-10 00:22:56 +0500

    Задача IPr/68

---
+++ Ipr_68/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.WriteLine("Введите положительные числа: M и N.");
+int m = InputInt("Введите M: ");
+if (m < 0)
+{
+    Console.Write("Ошибка M. Давай по новой");
+    return;
+}
+int n = InputInt("Введите N: ");
+if (n < 0)
+{
+    Console.Write("Ошибка N. Давай по новой");
+    return;
+}
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
